8cd255beae0f5a48f5445b8131c12859e7c3ab76
Super Admin <admin@streamvzla.com>
2026-07-19 21:20:45 -0400

    Remove debug, test and temp scripts from repo. Update .gitignore with permanent exclusion patterns

--- test_cols.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-print_r(Illuminate\Support\Facades\Schema::getColumnListing('email_accounts'));

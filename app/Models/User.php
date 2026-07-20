1bd8aafa770803a13e3d5910019ab5d2152a4292
Super Admin <admin@streamvzla.com>
2026-07-19 20:57:41 -0400

    Restore password hashed cast in User model - real bug was case-sensitive email

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'password'          => 'hashed',
             'is_active'         => 'boolean',
             'last_login_at'     => 'datetime',
             'subscription_ends_at' => 'datetime',
